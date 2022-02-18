61641d1bad9009dd42a729355a66130938cc54b6
shkwon98 <shkwon98@hanyang.ac.kr>
2022-02-18 13:35:02 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Simple Open EtherCAT Master Library
+[![Build Status](https://github.com/OpenEtherCATsociety/SOEM/workflows/build/badge.svg?branch=master)](https://github.com/OpenEtherCATsociety/SOEM/actions?workflow=build)
+
+BUILDING
+========
+
+
+Prerequisites
+-------------------------------
+
+ * CMake 3.9 or later
+
+
+Linux & macOS
+--------------
+
+   * `mkdir build`
+   * `cd build`
+   * `cmake ..`
+   * `make`
+
+
+Documentation
+-------------
+
+See https://openethercatsociety.github.io/doc/soem/
---
+++ build/test/epos4/CMakeFiles/epos4.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/pi/Main_Project/SOEM/test/epos4/epos4.c" "/home/pi/Main_Project/SOEM/build/test/epos4/CMakeFiles/epos4.dir/epos4.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../soem"
+  "../osal"
+  "../oshw"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/pi/Main_Project/SOEM/build/CMakeFiles/soem.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
